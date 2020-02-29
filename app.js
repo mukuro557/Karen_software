21058e4aae8be667f3f21fad5577004ebac47677
caesars335 <6131302003@lamduan.mfu.ac.th>
2020-03-01 01:43:31 +0700

    Add 10 API

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const body_parser = require("body-parser");
 const multer = require("multer");
 const mysql = require("mysql");
-const config = require("./dbConfig.js/index.js.js");
+const config = require("./dbConfig.js");
 
 //=========Put to use==========
 const app = express();
@@ -85,6 +85,147 @@ app.get("/page/maindataTable", function (req, res) {
     res.sendFile(path.join(__dirname, "/mainDataTable.html"))
 });
 
+//================== Services (functions) ===================
+
+// Load inspected item number by the user with lost status
+app.get("/user/profile/inspectedItem/status/lost/:Email_Importer", function (req, res) {
+    const Email_Importer = req.params.Email_Importer;
+    const sql = "SELECT count(status) FROM item WHERE status=0 AND Email_Importer=?;"
+
+    con.query(sql, [Email_Importer], function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load inspected item number by the user with repair status
+app.get("/user/profile/inspectedItem/status/repair/:Email_Importer", function (req, res) {
+    const Email_Importer = req.params.Email_Importer;
+    const sql = "SELECT count(status) FROM item WHERE status=2 AND Email_Importer=?;"
+
+    con.query(sql, [Email_Importer], function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load inspected item number by the user with normal status
+app.get("/user/profile/inspectedItem/status/normal/:Email_Importer", function (req, res) {
+    const Email_Importer = req.params.Email_Importer;
+    const sql = "SELECT count(status) FROM item WHERE status=1 AND Email_Importer=?;"
+
+    con.query(sql, [Email_Importer], function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load inspected item number by the user
+app.get("/user/profile/inspectedItem/number/:Email_Importer", function (req, res) {
+    const Email_Importer = req.params.Email_Importer;
+    const sql = "SELECT count(Email_Importer) FROM `item` WHERE Email_Importer=?;"
+
+    con.query(sql, [Email_Importer], function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load email of user
+app.get("/user/index/info/emailUser/:Email_user", function (req, res) {
+    const Email_user = req.params.Email_user;
+    const sql = "SELECT Email_user FROM `year_user` WHERE Email_user=?;"
+
+    con.query(sql, [Email_user], function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load inspected item by the user
+app.get("/user/profile/inspectedItem/info/:Email_user", function (req, res) {
+    const Email_user = req.params.Email_user;
+    const sql = "select IMAGE,Inventory_Number,Asset_Description,Model,Date_Scan,Cost_center,Location,Status from item where Email_Importer=?;"
+
+    con.query(sql, [Email_user], function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load all item info
+app.get("/item/dashboard/showAllInfo", function (req, res) {
+    const sql = "select IMAGE,Inventory_Number,Location,Received_date,Original_value,Department,Date_Scan,Email_Importer,Status from item"
+
+    con.query(sql, function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load item number with lost status
+app.get("/item/dashboard/number/status/lost", function (req, res) {
+    const sql = "SELECT count(status) FROM item WHERE status=0;"
+
+    con.query(sql,  function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load item number with repair status
+app.get("/item/dashboard/number/status/repair", function (req, res) {
+    const sql = "SELECT count(status) FROM item WHERE status=2;"
+
+    con.query(sql,  function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+// Load item number with normal status
+app.get("/item/dashboard/number/status/normal", function (req, res) {
+    const sql = "SELECT count(status) FROM item WHERE status=1;"
+
+    con.query(sql,  function (err, result, fields) {
+        if (err) {
+            res.status(503).send("DB error");
+        } else {
+            res.json(result).send()
+        }
+    })
+});
+
+
+
+// ================ EXAMPLE Draft of the API =================
 app.put("/item/lost/edit", function (req, res) {
     const Email_user = req.body.Email_user;
     const Status = req.body.Status;
